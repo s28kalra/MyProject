6ebe7de3c0ad880345c843fda10628d42bff7fe7
sagar <sagar@sagar-PC>
2020-08-05 15:49:26 +0530

    Created Calculator Class

---
+++ EmployeeManagememtSystem/src/com/lti/Calculator.java
@@ -0,0 +1,14 @@
+package com.lti;
+
+public class Calculator {
+	
+	static public void add(int a,int b){
+		System.out.println("Sum is= "+(a+b));
+	}
+
+	public static void main(String[] args) {
+		add(10,20);
+
+	}
+
+}
