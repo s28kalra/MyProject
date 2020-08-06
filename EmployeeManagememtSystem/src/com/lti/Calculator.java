b0f4cd5c9bb4fec686dfcb90b91b861fb0628c80
sagar <sagar@sagar-PC>
2020-08-06 09:47:36 +0530

    added diff

--- EmployeeManagememtSystem/src/com/lti/Calculator.java
+++ EmployeeManagememtSystem/src/com/lti/Calculator.java
@@ -5,6 +5,10 @@ public class Calculator {
 	static public void add(int a,int b){
 		System.out.println("Sum is= "+(a+b));
 	}
+	
+	static public void diff(int a,int b){
+		System.out.println("Diff is= "+(a-b));
+	}
 
 	public static void main(String[] args) {
 		add(10,20);
